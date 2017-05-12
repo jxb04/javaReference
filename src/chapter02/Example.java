c46f9402cca7b5b0332c75022cff3091c3a3c146
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-12 12:04:56 -0400

    Add files via upload
    Add files via upload

---
+++ src/chapter02/Example.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+// This is a simple java program.
+
+public class Example {
+	// your program begins with a call to main().
+	public static void main(String args[]) {
+		System.out.println("This is a simple java program.");
+	}
+
+}
