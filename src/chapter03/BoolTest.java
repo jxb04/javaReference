9a30636d022fda0fca0dd04fce25f309f598a4cb
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-15 09:20:11 -0400

    boolean type demo

--- src/chapter03/BoolTest.java
+++ src/chapter03/BoolTest.java
@@ -19,7 +19,7 @@ public class BoolTest {
 	if (b) System.out.println("This is not executed.");
 	
 	// outcome of a relational operator is a boolean value
-	System.out.println("10 > 8 is " + (10 > 8));
+	System.out.println("10 > 9 is " + (10 > 9));
 
 	}
 }
