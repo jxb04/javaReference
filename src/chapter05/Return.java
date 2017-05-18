7df59704eb8c361ab0e1a501b4ae1a6dd677cd91
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-18 16:17:38 -0400

    return statement

---
+++ src/chapter05/Return.java
@@ -0,0 +1,14 @@
+package chapter05;
+// Demonstrate return
+public class Return {
+	public static void main (String args[]) {
+		boolean t = true;
+		
+		System.out.println("Before the return");
+		
+		if (t) return; // return to caller
+		
+		System.out.println("this won't execute.");
+	}
+
+}
