a88e0fc0479a3af3a9fc39ad870e1f2d9e32edce
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-18 11:00:59 -0400

    declare loop control variable inside the loop

--- src/chapter05/ForTick.java
+++ src/chapter05/ForTick.java
@@ -2,9 +2,8 @@ package chapter05;
 // demonstrate the for loop
 public class ForTick {
 	public static void main (String args[]) {
-		int n;
 		
-		for (n = 10; n > 0; n--)
+		for (int n = 10; n > 0; n--)
 			System.out.println("tick " + n);
 	}
 
