90d1e8afa169c89468e3b5ac841ebf2d518cbdb0
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-18 16:08:37 -0400

    break within loop

---
+++ src/chapter05/Break.java
@@ -0,0 +1,20 @@
+package chapter05;
+// using break as a civilized form of goto
+public class Break {
+	public static void main(String args[]) {
+		boolean t = true;
+		
+		first: {
+			second: {
+			    third: {
+			        System.out.println("Before the break.");
+			        if (t) break second;   // break out of the second block
+			        System.out.println("This won't execute.");
+		        }
+		        System.out.println("This won't execute.");
+		    }
+		    System.out.println("This is after the second block.");
+		}
+	}
+
+}
