50de8d1c0e9c21fcb5ea5519a3733d4c1e57c99c
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-18 11:28:03 -0400

    parts of for loop can be empty

---
+++ src/chapter05/ForVar.java
@@ -0,0 +1,16 @@
+package chapter05;
+// parts of the for loop can be empty
+public class ForVar {
+	public static void main (String args[]) {
+		int i;
+		boolean done = false;
+		
+		i = 0;
+		for ( ; !done; ) {
+			System.out.println("i is " + i);
+			if (i == 10) done = true;
+			i++;
+		}
+	}
+
+}
