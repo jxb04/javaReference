c342bfd61a4b57cad49849e8ca807c544ed8c0fd
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-22 10:03:09 -0400

    added method to set dimensions

--- src/chapter06/Box.java
+++ src/chapter06/Box.java
@@ -9,4 +9,11 @@ class Box {
 	double volume() {
 		return width * height * depth;
 	}
+	
+	// sets dimensions of box
+	void setDim(double w, double h, double d) {
+		width = w;
+		height = h;
+		depth = d;
+	}
 }
