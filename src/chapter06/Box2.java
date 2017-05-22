0d723bfbc1ac5de9d58c3ab3ab91437c11db727d
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-22 14:28:37 -0400

    create a new class for parameterized constructor exclusively for BoxDemo7

---
+++ src/chapter06/Box2.java
@@ -0,0 +1,27 @@
+package chapter06;
+
+class Box2 {
+	double width;
+	double height;
+	double depth;
+	
+    // This is the constructor for Box.
+	Box2(double w, double h, double d) {
+		System.out.println("Constructing Box");
+		width = w;
+		height = h;
+		depth = d;
+	}
+	
+	// compute and return volume
+	double volume() {
+		return width * height * depth;
+	}
+	
+	// sets dimensions of box
+	void setDim(double w, double h, double d) {
+		width = w;
+		height = h;
+		depth = d;
+	}
+}
--- src/chapter06/BoxDemo7.java
+++ src/chapter06/BoxDemo7.java
@@ -3,8 +3,8 @@ package chapter06;
 public class BoxDemo7 {
 	public static void main(String args[]) {
 		// declare, allocate, and initialize Box objects
-		Box mybox1 = new Box(10, 20 , 15);
-		Box mybox2 = new Box(3, 6, 9);
+		Box2 mybox1 = new Box2(10, 20 , 15);
+		Box2 mybox2 = new Box2(3, 6, 9);
 		
 		double vol;
 		
