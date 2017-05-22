1d7106e166bf2a353855072ef5a941b1772e0ea0
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-22 14:24:28 -0400

    overload a method within a class

---
+++ src/chapter07/OverloadDemo.java
@@ -0,0 +1,24 @@
+package chapter07;
+// Demonstrate method overloading
+public class OverloadDemo {
+	void test() {
+		System.out.println("No parameters");
+	}
+	
+	// overload test for one integer parameter
+	void test(int a) {
+		System.out.println("a: " + a);
+	}
+	
+	// overload test for two integer parameters
+	void test(int a, int b) {
+		System.out.println("a and b: " + a + " " + b);
+	}
+	
+	// overload test for a double parameter
+	double test(double a) {
+		System.out.println("double a: " + a);
+		return a*a;
+	}
+
+}
