f6f691829c8ac97a55f0a1509cda51daceee5c55
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-24 08:26:02 -0400

    test object

---
+++ src/chapter07/Test.java
@@ -0,0 +1,17 @@
+package chapter07;
+// objects may be passed to methods
+public class Test {
+	int a, b;
+	
+	Test (int i, int j) {
+		a = i;
+		b = j;
+	}
+	
+	// return true if o is equal to the invoking object
+	boolean equalTo (Test o) {
+		if (o.a == a && o.b == b) return true;
+		else return false;
+	}
+
+}
