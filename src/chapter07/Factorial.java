f0294036c84f53536d19f14192a1ddebc982aeb7
Joel Bernasky <joel_bernasky@hotmail.com>
2017-05-24 14:02:20 -0400

    class to compute factorial

---
+++ src/chapter07/Factorial.java
@@ -0,0 +1,13 @@
+package chapter07;
+// A simple example of recursion
+public class Factorial {
+	// this is a recursive method
+	int fact(int n) {
+		int result;
+		
+		if (n == 1) return 1;
+		result = fact(n - 1) * n;
+		return result;
+	}
+
+}
