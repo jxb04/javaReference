dc7ae2508620a143dcaf265a8cb872f440efab95
Joel Bernasky <joel_bernasky@hotmail.com>
2017-06-08 08:39:15 -0400

    figure class

---
+++ src/chapter08/Figure.java
@@ -0,0 +1,17 @@
+package chapter08;
+//using run time polymorphism
+public class Figure {
+	double dim1;
+	double dim2;
+	
+	Figure (double a, double b) {
+		dim1 = a;
+		dim2 = b;
+	}
+	
+	double area () {
+		System.out.println("Area for Figure is undefined.");
+		return 0;
+	}
+
+}
