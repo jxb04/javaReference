2e1b3bef011ecd89ebbeef91771406da470f0545
Joel Bernasky <joel_bernasky@hotmail.com>
2017-06-08 10:52:17 -0400

    implement abstract class

---
+++ src/chapter08/Triangle2.java
@@ -0,0 +1,13 @@
+package chapter08;
+
+public class Triangle2 extends Figure2 {
+	Triangle2 (double a, double b) {
+		super (a, b);
+	}
+	
+	double area() {
+		System.out.println("Inside Area for Triangle.");
+		return dim1 * dim2 / 2;
+	}
+
+}
