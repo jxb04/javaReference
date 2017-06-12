ce80130b663e8e334a7694332e5c8ddbccf05749
Joel Bernasky <joel_bernasky@hotmail.com>
2017-06-12 13:20:20 -0400

    subclass of Protection in the same package

---
+++ src/chapter09/p1/Derived.java
@@ -0,0 +1,16 @@
+package chapter09.p1;
+
+class Derived extends Protection {
+	Derived() {
+		System.out.println("derived constructor");
+		System.out.println("n = " + n);
+		
+        // class only can access private variable
+		// System.out.println("n_pri " + n_pri);
+		
+		System.out.println("n_pro = " + n_pro);
+		System.out.println("n_pub = " + n_pub);
+		
+	}
+
+}
