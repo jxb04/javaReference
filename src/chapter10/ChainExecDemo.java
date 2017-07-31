f2634a286147b9abd914dfab136024bf2e205d61
Joel Bernasky <joel_bernasky@hotmail.com>
2017-07-31 16:25:10 -0400

    chained exceptions

---
+++ src/chapter10/ChainExecDemo.java
@@ -0,0 +1,29 @@
+package chapter10;
+
+// Demonstrate exception chaining
+
+public class ChainExecDemo {
+	static void demoproc() {
+		
+		// create an exception
+		NullPointerException e = new NullPointerException("top layer");
+		
+		// add a cause
+		e.initCause(new ArithmeticException("cause"));
+		
+		throw e;
+	}
+	
+	public static void main(String args[]) {
+		try {
+			demoproc();
+		}  catch(NullPointerException e) {
+			// display top level exception
+			System.out.println("Caught: " + e);
+			
+			// display cause exception
+			System.out.println("Original cause: " + e.getCause());
+		}
+	}
+
+}
