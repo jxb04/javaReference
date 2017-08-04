0aaab6147d3313fa758bedced7ffdcc699ad38b2
Joel Bernasky <joel_bernasky@hotmail.com>
2017-08-04 15:23:24 -0400

    synchronized block

--- src/chapter11/Callme.java
+++ src/chapter11/Callme.java
@@ -2,7 +2,7 @@ package chapter11;
 
 public class Callme {
 	// add synchronized keyword to prevent race condition
-	synchronized void call (String msg) {
+	void call (String msg) {
 		System.out.print("[" + msg);
 		try {
 			Thread.sleep(1000);
