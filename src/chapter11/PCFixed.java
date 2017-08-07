0431052e41720160e99b615915c3338fbd41d823
Joel Bernasky <joel_bernasky@hotmail.com>
2017-08-07 09:44:00 -0400

    interprocess communication

---
+++ src/chapter11/PCFixed.java
@@ -0,0 +1,13 @@
+package chapter11;
+
+public class PCFixed {
+	public static void main (String args[]) {
+		Q q = new Q();
+		new Producer(q);
+		new Consumer(q);
+		
+		System.out.println("Press Control-C to stop.");
+		
+	}
+
+}
