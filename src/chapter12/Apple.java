f0e905f1731f2f22203675b9ff74b68b43ca37a1
Joel Bernasky <joel_bernasky@hotmail.com>
2017-08-08 16:05:56 -0400

    enumeration as class type

--- src/chapter12/Apple.java
+++ src/chapter12/Apple.java
@@ -3,6 +3,13 @@ package chapter12;
 // and enumeration of apple varieties.
 
 public enum Apple {
-	Jonathan, GoldenDel, RedDel, Winesap, Cortland
+	Jonathan(10), GoldenDel(9), RedDel(12), Winesap(15), Cortland(8);
+	
+	private int price;  // price of each apple
+	
+	// Constructor
+	Apple (int p)  { price = p; }
+	
+	int getPrice() { return price; }
 	
 }
