f356cc9589178263c7ba01c9dbd78d4bf847fd33
Joel Bernasky <joel_bernasky@hotmail.com>
2017-08-10 08:47:20 -0400

    type wrapper

---
+++ src/chapter12/Wrap.java
@@ -0,0 +1,13 @@
+package chapter12;
+
+public class Wrap {
+	public static void main (String args[]) {
+		
+		Integer iOb = new Integer(100);
+		
+		int i = iOb.intValue();
+		
+		System.out.println(i + " " + iOb);  // displays 100 100
+	}
+
+}
