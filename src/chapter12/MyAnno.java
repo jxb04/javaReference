4ea1d74378547a680cf18bc98339d910d7119fb4
Joel Bernasky <joel_bernasky@hotmail.com>
2017-08-11 11:29:12 -0400

    simple annotation

---
+++ src/chapter12/MyAnno.java
@@ -0,0 +1,12 @@
+package chapter12;
+
+import java.lang.annotation.*;
+
+
+// A simple annotation type
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnno {
+	String str();
+	int val();
+	
+}
