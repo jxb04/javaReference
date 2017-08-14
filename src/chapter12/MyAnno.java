1e519cf4f46d70bcbb35fc710c1f60b6a07d862b
Joel Bernasky <joel_bernasky@hotmail.com>
2017-08-14 15:56:40 -0400

    default values for annotations

--- src/chapter12/MyAnno.java
+++ src/chapter12/MyAnno.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 // A simple annotation type
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnno {
-	String str();
-	int val();
+	String str() default "Testing";
+	int val() default 9000;
 	
 }
