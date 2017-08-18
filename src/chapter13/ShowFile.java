ff5373c5b7842af3b885eadf9700b4f811f37f3c
Joel Bernasky <joel_bernasky@hotmail.com>
2017-08-18 16:26:11 -0400

    display a files contents

---
+++ src/chapter13/ShowFile.java
@@ -0,0 +1,47 @@
+package chapter13;
+
+// display a text file.  To use this program, specify the name of the file that you want to see.
+// For example, to see a file called TEST.TXT, use the following command line.
+// java ShowFile TEST.TXT
+
+import java.io.*;
+
+public class ShowFile {
+	public static void main (String args[]) {
+		int i;
+		FileInputStream fin;
+		
+		// First, confirm that a filename has been specified
+		if (args.length != 1) {
+			System.out.println("Usage: ShowFile filename");
+			return;
+		}
+		
+		// Attempt to open the file.
+		try {
+			fin = new FileInputStream(args[0]);
+		} catch (FileNotFoundException e) {
+			System.out.println("Cannot open file.");
+			return;
+		}
+		
+		// At this point, the file is open and can be read
+		// Read characters until EOF is encountered
+		try {
+			do {
+				i = fin.read();
+				if (i != -1) System.out.print((char) i);
+			} while (i != -1);
+			} catch (IOException e) {
+				System.out.println("Error reading file.");
+			}
+			
+			// close the file
+			try {
+				fin.close();
+			} catch (IOException e) {
+				System.out.println("Error closing file.");
+			}
+	}
+
+}
