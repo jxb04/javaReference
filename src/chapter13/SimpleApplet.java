cf0e5b9d2b6ad4138f5febd12d52ec795b7cb0ab
Joel Bernasky <joel_bernasky@hotmail.com>
2017-08-21 15:15:57 -0400

    applets

---
+++ src/chapter13/SimpleApplet.java
@@ -0,0 +1,16 @@
+package chapter13;
+
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="SimpleApplet" width=200 height=60>
+</applet>
+ */
+public class SimpleApplet extends Applet {
+	public void paint (Graphics g) {
+		g.drawString("A simple applet", 20, 20);
+	}
+	
+
+}
