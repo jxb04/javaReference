9fee256f5c588a3dc3ad5cae5dd9253a41eadcba
Joel Bernasky <joel_bernasky@hotmail.com>
2017-09-07 16:35:20 -0400

    Bounded Wildcards

---
+++ src/chapter14/FourD.java
@@ -0,0 +1,13 @@
+package chapter14;
+
+// Four dimenzional coordinates.
+public class FourD extends ThreeD {
+	int t;
+	
+	FourD (int a, int b, int c, int d) {
+		super(a, b, c);
+		
+		t = d;
+	}
+
+}
