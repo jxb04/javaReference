cd84044f5e3dc91a650a81c431a2184ba6bfe01a
Joel Bernasky <joel_bernasky@hotmail.com>
2017-09-13 15:53:53 -0400

    non-generic class

---
+++ src/chapter14/NonGen2.java
@@ -0,0 +1,17 @@
+package chapter14;
+
+// A non-generic class can be the superclass of a generic class
+
+// A non-generic class
+public class NonGen2 {
+	int num;
+	
+	NonGen2(int i) {
+		num = i;
+	}
+	
+	int getnum() {
+		return num;
+	}
+
+}
