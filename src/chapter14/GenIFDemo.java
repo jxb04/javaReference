9141460c3a35be8c19d10210ae579355df539b36
Joel Bernasky <joel_bernasky@hotmail.com>
2017-09-13 10:16:46 -0400

    generic interface demo

---
+++ src/chapter14/GenIFDemo.java
@@ -0,0 +1,20 @@
+package chapter14;
+
+public class GenIFDemo {
+	public static void main (String args[]) {
+		Integer inums[] = { 2, 6, 2, 8, 6 };
+		Character chs[] = { 'b', 'r', 'p', 'w' };
+		
+		MyClass<Integer> iob = new MyClass<Integer>(inums);
+		MyClass<Character> cob = new MyClass<Character>(chs);
+		
+		System.out.println("Min value in inums: " + iob.min());
+		System.out.println("Max value in inums: " + iob.max());
+		
+		System.out.println();
+		
+		System.out.println("Min value in chs: " + cob.min());
+		System.out.println("Max value in chs: " + cob.max());
+	}
+
+}
