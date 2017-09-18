289f09e12a0dd071cc8dbcb8682999ee96906df0
Joel Bernasky <joel_bernasky@hotmail.com>
2017-09-18 15:03:35 -0400

    block lambda statement

---
+++ src/chapter15/BlockLambdaDemo.java
@@ -0,0 +1,19 @@
+package chapter15;
+
+public class BlockLambdaDemo {
+	public static void main (String args[]) {
+		// This block lambda computes the factorial of an int value.
+		NumericFunc factorial = (n) -> {
+			int result = 1;
+			
+			for(int i = 1; i <= n; i++) 
+				result = i * result;
+			
+			return result;
+		};
+		
+		System.out.println("The factorial of 3 is " + factorial.func(3));
+		System.out.println("The factorial of 5 is " + factorial.func(5));
+	}
+
+}
