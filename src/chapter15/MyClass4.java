a8c4ddcad358d0e71d1a7d536451511f25b3c20a
Joel Bernasky <joel_bernasky@hotmail.com>
2017-10-02 15:56:51 -0400

    reference to constructors

---
+++ src/chapter15/MyClass4.java
@@ -0,0 +1,14 @@
+package chapter15;
+
+public class MyClass4 {
+	private int val;
+	
+	// This constructor takes an argument.
+	MyClass4(int v) { val = v; }
+	
+	// This is the default contructor
+	MyClass4() { val = 0; }
+	
+	int getVal() { return val; }
+
+}
