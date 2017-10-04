c74c752fa0b0515cc27258e9d9d20d874f2fa899
Joel Bernasky <joel_bernasky@hotmail.com>
2017-10-04 15:27:48 -0400

    create string

---
+++ src/chapter16/MakeString.java
@@ -0,0 +1,15 @@
+package chapter16;
+
+// construct one string from another
+
+public class MakeString {
+	public static void main (String args[]) {
+		char c[] = { 'J', 'a', 'v', 'a' };
+		String s1 = new String (c);
+		String s2 = new String(s1);
+		
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+
+}
